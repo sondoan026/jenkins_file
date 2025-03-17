b76511a6e7e60acf0efe2830884fe440bffd5465
SonDC <sondoan026@gmail.com>
2025-03-17 16:14:21 +0700

    refactor

--- 10_10_10_153/lms/tai_lieu_y_khoa_jenkins.groovy
+++ 10_10_10_153/lms/tai_lieu_y_khoa_jenkins.groovy
@@ -43,7 +43,9 @@ pipeline {
                         sudo chmod -R 755 ${APP_PATH}
 
 
-                        rsync -avz --delete ${env.WORKSPACE}/. ${APP_PATH}/
+                        rsync -avz --delete ${env.WORKSPACE}/. ${APP_PATH}/ --log-file=/tmp/rsync.log
+                        echo "RSYNC LOG:"
+                        cat /tmp/rsync.log
 
                         cd ${APP_PATH}
 
