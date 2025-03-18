aeb79baf13b58ad6ee96fcc4a39c7d0375dc8060
SonDC <sondoan026@gmail.com>
2025-03-18 10:05:07 +0700

    refactor

--- 10_10_10_153/lms/tai_lieu_y_khoa_jenkins.groovy
+++ 10_10_10_153/lms/tai_lieu_y_khoa_jenkins.groovy
@@ -49,6 +49,7 @@ pipeline {
                         chmod -R 777 entrypoint.sh
                         chmod -R 777 data pg_data etc
 
+
                         docker-compose up -d
 
                         curl -X POST \
